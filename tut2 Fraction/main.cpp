1962a7de698109cacc58178d0e4c37748db7a3a5
BushK <Kmabusha2@gmail.com>
2016-02-28 22:02:08 +0200

    creating class definition

---
+++ tut2 Fraction/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+using namespace std;
+
+class Fraction
+{
+private:
+	//Declaring private data members
+	int Denom;//Denominator for fraction
+	int Num; //Numerator for fraction
+public:
+//Prototypes for public member functions
+};
